07187caddb4ef5dead0bdaa5fa0f519d9eddb414
Smeltier <connect.goncalves@gmail.com>
2025-11-18 20:25:26 -0300

    add: finalização da base do Pac-Man.

--- src/entities/entity.py
+++ src/entities/entity.py
@@ -3,5 +3,16 @@ import pygame
 class Entity ():
 
     def __init__(self, x, y, environment):
-        self.position = pygame.Vector2((x, y))
+        
+        self._position = pygame.Vector2((x, y))
         self._ENVIRONMENT = environment
+
+    # MÉTODOS PÚBLICOS
+
+    @property
+    def position(self):
+        return self._position
+    
+    @position.setter
+    def position(self, new_position):
+        self._position = new_position
