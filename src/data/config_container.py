ccb00e508386fd28568c8e1a7848af3e6f9c4940
Smeltier <connect.goncalves@gmail.com>
2025-12-04 17:51:32 -0300

    add: adiciona as classes de configurações dos fantasmas ao ConfigContainer.

--- src/data/config_container.py
+++ src/data/config_container.py
@@ -1,19 +1,27 @@
-from src.core.settings import Settings
+from src.core.settings                          import Settings
 from src.data.class_config.audio_manager_config import AudioManagerConfig
-from src.data.class_config.environment_config import EnvironmentConfig
-from src.data.class_config.ghost_config import GhostConfig
-from src.data.class_config.hud_config import HUDConfig
-from src.data.class_config.pacman_config import PacmanConfig
-from src.data.class_config.maze_config import MazeConfig
+from src.data.class_config.environment_config   import EnvironmentConfig
+from src.data.class_config.ghost_config         import GhostConfig
+from src.data.class_config.hud_config           import HUDConfig
+from src.data.class_config.pacman_config        import PacmanConfig
+from src.data.class_config.maze_config          import MazeConfig
+from src.data.class_config.blinky_config        import BlinkyConfig
+from src.data.class_config.clyde_config         import ClydeConfig
+from src.data.class_config.inky_config          import InkyConfig
+from src.data.class_config.pinky_config         import PinkyConfig
 
 class ConfigContainer ():
 
     def __init__ (self) -> None:
         settings = Settings("src/data/settings/config.json")
 
-        self.pacman = PacmanConfig(settings)
-        self.ghost = GhostConfig(settings)
-        self.environment = EnvironmentConfig(settings)
-        self.hud = HUDConfig(settings)
         self.audio_manager = AudioManagerConfig(settings)
-        self.maze = MazeConfig(settings)
+        self.environment   = EnvironmentConfig(settings)
+        self.pacman        = PacmanConfig(settings)
+        self.blinky        = BlinkyConfig(settings)
+        self.ghost         = GhostConfig(settings)
+        self.pinky         = PinkyConfig(settings)
+        self.clyde         = ClydeConfig(settings)
+        self.maze          = MazeConfig(settings)
+        self.inky          = InkyConfig(settings)
+        self.hud           = HUDConfig(settings)
