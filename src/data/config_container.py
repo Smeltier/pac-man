87340573b2cf11b3b7816bbad81b25a24980da28
Smeltier <connect.goncalves@gmail.com>
2025-12-04 17:01:31 -0300

    att: atualiza imports na classe ConfigContainer

--- src/data/config_container.py
+++ src/data/config_container.py
@@ -1,10 +1,10 @@
 from src.core.settings import Settings
-from src.data.audio_manager_config import AudioManagerConfig
-from src.data.environment_config import EnvironmentConfig
-from src.data.ghost_config import GhostConfig
-from src.data.hud_config import HUDCOnfig
-from src.data.pacman_config import PacmanConfig
-from src.data.maze_config import MazeConfig
+from src.data.class_config.audio_manager_config import AudioManagerConfig
+from src.data.class_config.environment_config import EnvironmentConfig
+from src.data.class_config.ghost_config import GhostConfig
+from src.data.class_config.hud_config import HUDConfig
+from src.data.class_config.pacman_config import PacmanConfig
+from src.data.class_config.maze_config import MazeConfig
 
 class ConfigContainer ():
 
@@ -14,6 +14,6 @@ class ConfigContainer ():
         self.pacman = PacmanConfig(settings)
         self.ghost = GhostConfig(settings)
         self.environment = EnvironmentConfig(settings)
-        self.hud = HUDCOnfig(settings)
+        self.hud = HUDConfig(settings)
         self.audio_manager = AudioManagerConfig(settings)
         self.maze = MazeConfig(settings)
